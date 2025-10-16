9572bb798c14034d079f876da8421015d1f56ad8
devzeuz <soceeenin@yahoo.com>
2025-10-16 03:41:20 -0700

    changes

--- main.tf
+++ main.tf
@@ -1,16 +1,11 @@
-resource "tls_private_key" "example" {
+resource "tls_private_key" "key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
 resource "aws_key_pair" "generated_key" {
   key_name   = "generated-key"
-  public_key = tls_private_key.example.public_key_openssh
-}
-
-output "private_key" {
-  value     = tls_private_key.example.private_key_pem
-  sensitive = true
+  public_key = tls_private_key.key.public_key_openssh
 }
 
 resource "aws_security_group" "allow_ssh" {
@@ -56,4 +51,13 @@ resource "aws_instance" "hng_ec2_instance" {
   cp -r /tmp/site/* /usr/share/nginx/html/
   chown -R nginx:nginx /usr/share/nginx/html/
 EOF
+}
+
+output "private_key" {
+  value     = tls_private_key.key.private_key_pem
+  sensitive = true
+}
+
+output "instace_public_ip" {
+    value = aws_instance.hng_ec2_instance.public_ip
 }
