0b0891242b5b9af103e6fe12fbf39ef11e3619a9
devzeuz <soceeenin@yahoo.com>
2025-10-16 04:29:29 -0700

    SG-1

--- main.tf
+++ main.tf
@@ -20,47 +20,47 @@ resource "aws_vpc_security_group_ingress_rule" "http" {
   to_port           = 80
 }
 
-# resource "tls_private_key" "key" {
-#   algorithm = "RSA"
-#   rsa_bits  = 4096
-# }
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
 
-# resource "aws_key_pair" "generated_key" {
-#   key_name   = "generated-key"
-#   public_key = tls_private_key.key.public_key_openssh
-# }
+resource "aws_key_pair" "generated_key" {
+  key_name   = "generated-key"
+  public_key = tls_private_key.key.public_key_openssh
+}
 
-# resource "aws_instance" "hng_ec2_instance" {
-#   ami                         = "ami-0341d95f75f311023"
-#   instance_type               = "t2.micro"
-#   subnet_id                   = "subnet-0b0b5224c6bb51e97"
-#   key_name                    = aws_key_pair.generated_key.key_name
-#   vpc_security_group_ids      = [aws_security_group.allow_ssh.name]
-#   associate_public_ip_address = true
+resource "aws_instance" "hng_ec2_instance" {
+  ami                         = "ami-0341d95f75f311023"
+  instance_type               = "t2.micro"
+  subnet_id                   = "subnet-0b0b5224c6bb51e97"
+  key_name                    = aws_key_pair.generated_key.key_name
+  vpc_security_group_ids      = [aws_security_group.SG.name]
+  associate_public_ip_address = true
 
-#   tags = {
-#     Name = "hng_ec2_instance_"
-#   }
+  tags = {
+    Name = "hng_ec2_instance_"
+  }
 
-#   user_data = <<-EOF
-#   #!/bin/bash
-#   sudo yum update -y
-#   sudo yum install -y nginx
-#   sudo systemctl start nginx
-#   sudo systemctl enable nginx
+  user_data = <<-EOF
+  #!/bin/bash
+  sudo yum update -y
+  sudo yum install -y nginx
+  sudo systemctl start nginx
+  sudo systemctl enable nginx
 
-#   rm -rf /usr/share/nginx/html/*
-#   git clone https://github.com/devzeuz/hng13-stage0-devops.git /tmp/site
-#   cp -r /tmp/site/* /usr/share/nginx/html/
-#   chown -R nginx:nginx /usr/share/nginx/html/
-# EOF
-# }
+  rm -rf /usr/share/nginx/html/*
+  git clone https://github.com/devzeuz/hng13-stage0-devops.git /tmp/site
+  cp -r /tmp/site/* /usr/share/nginx/html/
+  chown -R nginx:nginx /usr/share/nginx/html/
+EOF
+}
 
-# output "private_key" {
-#   value     = tls_private_key.key.private_key_pem
-#   sensitive = true
-# }
+output "private_key" {
+  value     = tls_private_key.key.private_key_pem
+  sensitive = true
+}
 
-# output "instace_public_ip" {
-#     value = aws_instance.hng_ec2_instance.public_ip
-# }
+output "instace_public_ip" {
+    value = aws_instance.hng_ec2_instance.public_ip
+}
